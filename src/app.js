b57cf160b28bc7b7e1a11cfa7fd097b5c6b3048b
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2016-01-16 11:34:41 +0100

    [DisplayName of Component] Added displayName property to React Component class

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 
 export class App extends Component {
+  displayName = 'App';
+
   render() {
     return (
       <span>{'Hello from React component'}</span>
