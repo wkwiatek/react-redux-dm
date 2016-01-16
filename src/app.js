1794b0438e500b5cb9cf0c580156aaa0170b314f
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2016-01-16 12:20:03 +0100

    [Separated components] All code from App moved to 2 different components

--- src/app.js
+++ src/app.js
@@ -1,35 +1,16 @@
 import React, {Component} from 'react'
 
-let id = 0
-
-class Todo {
-  constructor(name = 'Unknown', description = ' - ') {
-    this.id = id++
-    this.name = name
-    this.description = description
-  }
-}
+import {TodoAdd} from './components/TodoAdd'
+import {TodoList} from './components/TodoList'
 
 export class App extends Component {
   displayName = 'App';
 
-  todos = [
-    new Todo('Learn React!', 'hype there is'),
-    new Todo()
-  ];
-
   render() {
     return (
       <div>
-        <form>
-          <input type="text"/>
-          <button>{'Add'}</button>
-        </form>
-        <ul>
-          {
-            this.todos.map(todo => <li key={todo.id}>{todo.name}{' ('}{todo.description}{') '}</li>)
-          }
-        </ul>
+        <TodoAdd/>
+        <TodoList/>
       </div>
     )
   }
